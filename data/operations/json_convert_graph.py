5b3582af07fd539298561b5e0b0a3d011253bebd
kbsshas <yyyy000215@outlook.com>
2025-12-24 23:49:48 +0800

    同上

--- data/operations/json_convert_graph.py
+++ data/operations/json_convert_graph.py
@@ -4,7 +4,7 @@ from preprocess.build_save_graph_with_alert_on_node import build_and_save_graph_
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sample", type=str, required=True, help="路径：原始 JSON 样本文件")
+    parser.add_argument("--sample", type=str, default="data\sample\merged\merged_sample_20250816_164358.json", help="路径：原始 JSON 样本文件")
     parser.add_argument("--output", type=str, default="data/graph/sample_2.pt", help="输出文件")
     parser.add_argument("--alert-on-edges", type=str, default='y', help='告警信息是否在边上')
     args = parser.parse_args()
