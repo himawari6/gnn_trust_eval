f81ad4d5de6b50753839f7924d1d3b5e44cb57ed
kbsshas <yyyy000215@outlook.com>
2025-12-25 11:57:53 +0800

    为了进行新的实验，增加了一种边的类型，新的图已构建完毕

--- test/build_toy_graph_test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-all_graphs = torch.load("data\\graph\\test.pt")
-data = all_graphs[457]
-save_path = "data\\graph\\toy_alert_on_node.pt"
-torch.save(data, save_path)
--- test/load_sample_from_db_test.py
+++
@@ -1,7 +0,0 @@
-import json
-from datetime import datetime
-from data.operations.db_sample_collector import get_features_from_db
-
-if __name__ == '__main__':
-    get_features_from_db()
-        
